a9c3350807b4020c29947e8aa023a37e6db560c0
dragon <1011151667@qq.com>
2015-10-21 17:42:51 +0800

    modefied some argues

--- javescript/index.js
+++ javescript/index.js
@@ -21,31 +21,31 @@ $('#newTaskOn').click(function(){
 
     //create menuitem
     sub1_1 = new gui.MenuItem({label:"导入JS"});
-    sub1_2= new gui.MenuItem({label:"导出JS"});
+    sub1_2 = new gui.MenuItem({label:"导出JS"});
     sub1_3 = new gui.MenuItem({label:"开启代理服务"});
-    sub1_4= new gui.MenuItem({label:"关闭代理服务"});
+    sub1_4 = new gui.MenuItem({label:"关闭代理服务"});
     sub1_5 = new gui.MenuItem({label:"数据库信息"});
 
     sub2_1 = new gui.MenuItem({label:"所有日志"});
-    sub2_2= new gui.MenuItem({label:"错误日志"});
+    sub2_2 = new gui.MenuItem({label:"错误日志"});
 
     sub3_1 = new gui.MenuItem({label:"所有作业"});
-    sub3_2= new gui.MenuItem({label:"正在运行的作业"});
+    sub3_2 = new gui.MenuItem({label:"正在运行的作业"});
     sub3_3 = new gui.MenuItem({label:"已停止作业"});
-    sub3_4= new gui.MenuItem({label:"未执行作业"});
+    sub3_4 = new gui.MenuItem({label:"未执行作业"});
     sub3_5 = new gui.MenuItem({label:"新建作业"});
-    sub3_6= new gui.MenuItem({label:"修改作业"});
+    sub3_6 = new gui.MenuItem({label:"修改作业"});
     sub3_7 = new gui.MenuItem({label:"删除作业"});
-    sub3_8= new gui.MenuItem({label:"查询作业"});
+    sub3_8 = new gui.MenuItem({label:"查询作业"});
     sub3_9 = new gui.MenuItem({label:"异常作业"});
 
     sub4_1 = new gui.MenuItem({label:"修改用户信息"});
-    sub4_2= new gui.MenuItem({label:"修改登录密码"});
+    sub4_2 = new gui.MenuItem({label:"修改登录密码"});
 
     sub5_1 = new gui.MenuItem({label:"帮助信息"});
-    sub5_2= new gui.MenuItem({label:"关于北电"});
+    sub5_2 = new gui.MenuItem({label:"关于北电"});
     sub5_3 = new gui.MenuItem({label:"退出软件"});
-    sub5_4= new gui.MenuItem({label:"软件版本"});
+    sub5_4 = new gui.MenuItem({label:"软件版本"});
     //let menu to the menubar
     menubar.append(new gui.MenuItem({ label: '系统设置', submenu: sub1 }));
     menubar.append(new gui.MenuItem({ label: '日志管理', submenu: sub2 }));
